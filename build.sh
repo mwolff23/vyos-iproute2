9a4bea292594bac0e29342278eb8da70db5ecac1
mwolff <mwolff@build.wlg.afx.ovh>
2024-08-31 22:38:11 +0200

    Add equuleus

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@ set -e
 
 SRC=iproute2
 
-git clone -b debian/bookworm-backports https://salsa.debian.org/kernel-team/iproute2.git "$SRC"
+git clone -b buster-backports https://salsa.debian.org/kernel-team/iproute2.git "$SRC"
 cd "$SRC"
 
-git checkout 12f618a8aa559e03ffb9ef332d1be517fd79c1c0
+git checkout f0c31ddcc9cbd3eba6b10373dcfbec9fedcadd04
 
 sudo mk-build-deps --install --tool "apt-get --yes --no-install-recommends"
 exec dpkg-buildpackage -b -us -uc -tc
